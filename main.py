acb6e7c923be7b280398fbbb74f5fdb9b9ede150
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-10-19 22:11:08 +0530

    added a function to install powerline for vim, along with that set line numbers for vim

--- main.py
+++ main.py
@@ -43,6 +43,21 @@ def add_powerline():
     file=open("~/.bashrc","a")
     file.write(content)
 
+def add_vim_powerline():
+    os.system("dnf install vim-powerline")
+    content = ( "\npython3 from powerline.vim import setup as powerline_setup"
+                "\npython3 powerline_setup()"
+                "\npython3 del powerline_setup"
+                '\nset laststatus=2 " Always display the statusline in all windows'
+                '\nset showtabline=2 " Always display the tabline, even if there is only one tab'
+                '\nset noshowmode " Hide the default mode text (e.g. -- INSERT -- below the statusline)'
+                '\nset t_Co=256'
+                '\nset number'
+                '\nset mouse=a'
+    )
+    with open("~/.vimrc","a") as fobj:
+        fobj.write(content)
+
 
 if __name__ == '__main__':
     update()
@@ -53,3 +68,4 @@ if __name__ == '__main__':
     install_java_plugins()
     install_spotify()
     add_powerline()
+    add_vim_powerline()
